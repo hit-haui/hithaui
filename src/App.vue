ca73c18542cedeb05197c3a564d69fbf102a2d9d
trankhacbinhduong <trankhacbinhduong@gmail.com>
2023-06-10 22:49:14 +0700

    :+1: Working with vue single file component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
+<script setup>
+const name = "Duo";
+</script>
 <template>
-  <div>Hello, world!</div>
+  <div class="home-page">
+    <h1 class="title">Hello, {{ name }}!</h1>
+  </div>
 </template>
+<style>
+.home-page > .title {
+  text-align: center;
+}
+</style>
