14e866eeb212c7c712ac6651adca1e6a7d890580
trankhacbinhduong <trankhacbinhduong@gmail.com>
2023-06-10 22:49:20 +0700

    :+1: Working with attribute binding

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <script setup>
-const name = "Duo";
+import hitLogo from "@/assets/hit-logo.jpeg";
 </script>
 <template>
   <div class="home-page">
-    <h1 class="title">Hello, {{ name }}!</h1>
+    <header class="header">
+      <div class="brand">
+        <img class="logo" :src="hitLogo" alt="HIT CLUB LOGO" />
+        <h1 class="title">HIT CLUB</h1>
+      </div>
+    </header>
   </div>
 </template>
 <style>
-.home-page > .title {
+.home-page > .header {
   text-align: center;
+  padding: 0 32px;
+  border-bottom: 1px solid #333;
+}
+
+.home-page > .header > .brand {
+  display: flex;
+  align-items: center;
+}
+
+.home-page > .header > .brand > .title {
+  font-weight: 600;
+  margin-left: 8px;
+}
+
+.home-page > .header > .brand > .logo {
+  width: 50px;
+  height: 50px;
 }
 </style>
