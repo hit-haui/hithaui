876d696c60294e98ad6ca68035e01aa5989e4597
trankhacbinhduong <trankhacbinhduong@gmail.com>
2023-06-11 18:29:44 +0700

    :tada: Init vue project

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # hithaui
+
+# create vue app
+
+```
+npm init vue@latest
+...
+```
+
+- API styles
+- SFC
+- template binding
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+const generationNumber = 13
+</script>
+
+<template>
+  <h1 class="title">HIT CLUB - {{ generationNumber }}</h1>
+</template>
+
+<style scoped>
+.title {
+  font-size: 36px;
+  font-weight: 600;
+  text-align: center;
+}
+</style>
