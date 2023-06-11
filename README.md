476382d13517eca26c934bef3e318a8c0b5c8237
trankhacbinhduong <trankhacbinhduong@gmail.com>
2023-06-11 18:46:48 +0700

    :+1: Working with attribute binding

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ npm init vue@latest
 - API styles
 - SFC
 - template binding
+
+# attribute binding
+
+- v-bind directive: v-bind:src
+- :src for short
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <script setup>
-const generationNumber = 13
+import hitLogo from './assets/hit-logo.jpeg'
 </script>
 
 <template>
-  <h1 class="title">HIT CLUB - {{ generationNumber }}</h1>
+  <div class="app-component">
+    <header class="header">
+      <div class="brand">
+        <img class="logo" :src="hitLogo" alt="HIT CLUB LOGO 13" />
+        <h1 class="title">HIT</h1>
+      </div>
+    </header>
+  </div>
 </template>
 
 <style scoped>
-.title {
-  font-size: 36px;
+.app-component > .header {
+  padding: 0 16px;
+  display: flex;
+  justify-content: flex-start;
+  border-bottom: 1px solid orange;
+}
+.app-component > .header > .brand {
+  display: flex;
+  align-items: center;
+  gap: 16px;
+}
+.app-component > .header > .brand > .logo {
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+}
+.app-component > .header > .brand > .title {
+  font-size: 32px;
   font-weight: 600;
   text-align: center;
+  color: orange;
 }
 </style>
