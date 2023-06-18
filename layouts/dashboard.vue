0adf17619b367f73eefe0a01b578faf5c123e1a0
Trần Khắc Bình Dương <tranduong3701@gmail.com>
2023-06-18 17:27:39 +0700

    :poop: Display user info feature (#18)

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
+import { useAuthStore } from '~/stores/auth'
 import HITLogo from '~/assets/images/logoHIT.png'
 
 const isSidebarCollapsed = ref(false)
 function handleToggleSidebar() {
   isSidebarCollapsed.value = !isSidebarCollapsed.value
 }
+
+const authStore = useAuthStore()
+const { username } = storeToRefs(authStore)
+await authStore.getUser()
 </script>
 
 <template>
@@ -21,6 +26,12 @@ function handleToggleSidebar() {
       <header class="header">
         <div class="sidebar-toggle" @click="handleToggleSidebar">
           <Icon name="uil:align" size="30" />
+        </div>
+        <div class="user-card">
+          <div class="avatar" />
+          <h4 class="username">
+            {{ username }}
+          </h4>
         </div>
       </header>
       <div class="content">
@@ -43,46 +54,71 @@ function handleToggleSidebar() {
 .dashboard-layout {
   display: flex;
   width: 100%;
-  > .sidebar {
+
+  >.sidebar {
     height: 100vh;
   }
-  > .sidebar  > .app-info {
+
+  >.sidebar>.app-info {
     display: flex;
     justify-content: flex-start;
     align-items: center;
     padding: 8px;
   }
 
-  > .sidebar > .app-info > .logo {
+  >.sidebar>.app-info>.logo {
     display: block;
     width: 50px;
   }
-  > .sidebar > .app-info > .name {
+
+  >.sidebar>.app-info>.name {
     font-size: 20px;
     color: $primary-color;
   }
-  > .main-content {
+
+  >.main-content {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     flex-grow: 1;
   }
-  > .main-content > .header {
+
+  >.main-content>.header {
     padding: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
-  > .main-content > .header > .sidebar-toggle {
+
+  >.main-content>.header>.sidebar-toggle {
     cursor: pointer;
   }
-  > .main-content > .content {
+
+  >.main-content>.header>.user-card {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+
+  >.main-content>.header>.user-card>.avatar {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-color: $primary-color;
+  }
+
+  >.main-content>.content {
     background-color: #e8e8e8;
     flex-grow: 1;
   }
-  > .main-content > .footer {
+
+  >.main-content>.footer {
     display: flex;
     justify-content: space-between;
     padding: 16px;
   }
-  > .main-content > .footer > .social-icons {
+
+  >.main-content>.footer>.social-icons {
     display: flex;
     gap: 8px;
   }
