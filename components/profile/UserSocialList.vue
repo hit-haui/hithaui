dfde8560ea9048aa2ee0b1af4c52aaa8d2f7c170
Trần Khắc Bình Dương <tranduong3701@gmail.com>
2023-07-11 17:50:54 +0700

    :lipstick: Implement ProfileUserSocialList component UI (#64)

---
+++ components/profile/UserSocialList.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { useAuthStore } from '~/stores/auth'
+
+const authStore = useAuthStore()
+const { user } = storeToRefs(authStore)
+
+const userSocials = computed(() => {
+  return {
+    facebook: user.value?.facebook,
+    linkedin: user.value?.linkedin,
+    github: user.value?.github,
+  }
+})
+</script>
+
+<template>
+  <div class="social-list">
+    <div class="content">
+      <h3 class="title">
+        Social accounts
+      </h3>
+      <ul class="list">
+        <ProfileUserSocialItem
+          v-for="(value, key) in userSocials"
+          :key="key"
+          :name="key"
+          :value="value"
+          class="item"
+        />
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.social-list {
+  > .content {
+    > .title {
+      @apply text-xl font-semibold;
+    }
+
+    > .list {
+      @apply divide-y divide-gray-200;
+    }
+
+    > .list > .item {
+      @apply py-4;
+    }
+  }
+}
+</style>
