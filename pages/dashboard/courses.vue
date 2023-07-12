8ef252e7afea349ea84ea2d3fbc687be413107b1
trankhacbinhduong <trankhacbinhduong@gmail.com>
2023-07-12 21:35:10 +0700

    :hammer: Add courses management page layout UI

---
+++ pages/dashboard/courses.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="courses-management-page">
+    <header class="header">
+      <h2 class="title">
+        Ours Courses
+      </h2>
+    </header>
+    <div class="toolbar" />
+    <div class="courses" />
+    <div class="footer" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.courses-management-page {
+
+  .header {
+    @apply p-4;
+  }
+
+  .header > .title {
+    @apply text-lg font-semibold text-left text-gray-900;
+  }
+
+  .header > .description {
+    @apply mt-1 text-sm font-normal text-gray-500;
+  }
+}
+</style>
